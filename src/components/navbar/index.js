ed2c3d6e538f9c83b11bbf7fd515a2f32e155734
J. Connor O'Brien <j.connor.obrien@gmail.com>
2022-03-23 16:43:03 -0400

    Wireframed with Flexbox Video

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import linkedin from '../../assets/icons/linkedin.png';
-import github from '../../assets/icons/github.png';
+import Icons from '../icons';
 
 function Navbar() {
     return(
-        <nav>
-            Hello World
+        <nav class="navbar">
+            <div class="container">
+                <ul class="nav">
+                    <li>
+                        <a href="#">Bio</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                    <li>
+                        <a id='resume' target="_blank" href="https://drive.google.com/file/d/1sY6MGYYcJ07u5dt5vdSd4UPRn-GGm4xU/view?usp=sharing" rel="noreferrer">
+                        Resume
+                        </a>
+                    </li>
+                </ul>
+                <Icons></Icons>
+            </div>
         </nav>
     )
 }
---
+++ src/components/showcase/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Project from '../project';
+
+function Showcase() {
+    return( 
+        <section class="boxes showcase">
+            <div class="container showcase-row-1">
+                <Project></Project>
+                <Project></Project>
+                <Project></Project>
+            </div>
+            <div class="container showcase-row-1">
+                <Project></Project>
+                <Project></Project>
+                <Project></Project>
+            </div>
+        </section>
+    )
+}
+
+export default Showcase;
