b324c922650993e71c235715ec20fccd8bd45af1
J. Connor O'Brien <j.connor.obrien@gmail.com>
2022-03-23 12:43:25 -0400

    Functioning Navbar Component

---
+++ src/components/navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import linkedin from '../../assets/icons/linkedin.png';
+import github from '../../assets/icons/github.png';
+
+function Navbar() {
+    return(
+        <nav>
+            Hello World
+        </nav>
+    )
+}
+
+export default Navbar;
